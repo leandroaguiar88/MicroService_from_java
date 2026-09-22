d506fc6e7e2d57d54b0d73ab64dd23f5ff5379b3
unknown <leoaguiarlima@gmail.com>
2026-09-21 21:06:04 -0300

    feat(hr-worker): configure Spring Cloud Config client and add Actuator for runtime refresh

--- hr-worker/src/main/java/com/leandrolima/hr_worker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/leandrolima/hr_worker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,16 +16,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.leandrolima.hr_worker.entities.Worker;
 import com.leandrolima.hr_worker.repositories.WorkerRepository;
 
+
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 	
 	private static Logger Logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 
 	@Autowired
 	private WorkerRepository repository;
+	
+
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		Logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 	
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
@@ -34,6 +47,7 @@ public class WorkerResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
 		Logger.info("PORT =" + env.getProperty("local.server.port"));
+		
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
