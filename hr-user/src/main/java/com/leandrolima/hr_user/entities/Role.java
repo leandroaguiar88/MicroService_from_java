0470c31ff2c09d6119d218717e81702a4eb69a48
unknown <leoaguiarlima@gmail.com>
2026-09-22 22:52:47 -0300

    User, Role, many-to-many association

---
+++ hr-user/src/main/java/com/leandrolima/hr_user/entities/Role.java
@@ -0,0 +1,66 @@
+package com.leandrolima.hr_user.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_role")
+
+public class Role implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	private String roleName;
+	
+	public Role() {
+		
+	}
+
+	public Role(Long id, String roleName) {
+		super();
+		Id = id;
+		this.roleName = roleName;
+	}
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id, roleName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(Id, other.Id) && Objects.equals(roleName, other.roleName);
+	}
+
+}
